c6f8c37cbeceb3b53638dab9f5ebefdce1927afd
David Turanski <dturanski@pivotal.io>
2017-05-17 12:02:03 -0400

    Update sentiment-service to implement pre and post processing

--- spring-cloud-stream-deployment-full/sentiment-service.py
+++ spring-cloud-stream-deployment-full/sentiment-service.py
@@ -16,26 +16,48 @@ from springcloudstream.stream import Processor
 
 warnings.filterwarnings("ignore")
 
-resp = requests.get("https://raw.githubusercontent.com/crawles/gpdb_sentiment_analysis_twitter_model/master/twitter_sentiment_model.pkl")
+resp = requests.get(
+    "https://raw.githubusercontent.com/crawles/gpdb_sentiment_analysis_twitter_model/master/twitter_sentiment_model.pkl")
 resp.raise_for_status()
 cl = cPickle.loads(resp.content)
 
+
 def regex_preprocess(raw_tweets):
-    pp_text = pd.Series(raw_tweets)
+    tweets = map(lambda t: t['text'], raw_tweets)
+    pp_text = pd.Series(tweets)
 
     user_pat = '(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)'
     http_pat = '(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})'
-    repeat_pat, repeat_repl = "(.)\\1\\1+",'\\1\\1'
+    repeat_pat, repeat_repl = "(.)\\1\\1+", '\\1\\1'
 
-    pp_text = pp_text.str.replace(pat = user_pat, repl = 'USERNAME')
-    pp_text = pp_text.str.replace(pat = http_pat, repl = 'URL')
-    pp_text.str.replace(pat = repeat_pat, repl = repeat_repl)
+    pp_text = pp_text.str.replace(pat=user_pat, repl='USERNAME')
+    pp_text = pp_text.str.replace(pat=http_pat, repl='URL')
+    pp_text.str.replace(pat=repeat_pat, repl=repeat_repl)
     return pp_text
 
+
+def post_process(polarities, tweets):
+    result = []
+    try:
+        for i, polarity in enumerate(polarities):
+            result.append({'polarity': polarity, 'text': tweets[i]})
+
+        return json.dumps(result)
+
+    except:
+        return json.dumps([])
+
+
 def process(data):
-    doc = json.loads(data)
-    X = regex_preprocess(doc["data"])
-    prediction = cl.predict_proba(X)[:][:, 1]
-    return json.dumps({"polarity" : prediction.tolist()})
+    # Array of raw tweets
+    raw_tweets = json.loads(str(data))
+    # Convert to {[text1,text2,...]}
+    tweets = regex_preprocess(raw_tweets)
+
+    ## run the prediction
+    prediction = cl.predict_proba(tweets)[:][:, 1]
+
+    return post_process(prediction.tolist(),tweets)
+
 
 Processor().start(process)
